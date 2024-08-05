dff5f4bc16fbed060e71b309f5b9dd0db60ccf09
Robson Ferreira de Fairas <robsonfdfarias@gmail.com>
2024-08-05 08:54:47 -0300

    Primeiro push. Começando o desenvolvimento do plugin de download.

---
+++ download-rff.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+Plugin Name: Slide image RFF
+Plugin URI: http://exemplo.com
+Description: Cria um sistema de administração de downloads.
+Version: 1.0
+Author: Robson Ferreira de Farias
+Email: robsonfdfarias@gmail.com
+Author URI: http://infocomrobson.com.br
+License: GPL2
+*/
+
+ //se chamar diretamente e não pelo wordpress, aborta
+ if(!defined('WPINC')){
+    die();
+ }
